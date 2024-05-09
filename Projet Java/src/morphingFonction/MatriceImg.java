654bf44ec1e50e0de9b7cc3b466df651a00b8803
R4ik0 <raiko352003@gmail.com>
2024-05-09 19:23:38 +0200

    creation image + debut construction matrice suivante

--- Projet Java/src/morphingFonction/MatriceImg.java
+++ Projet Java/src/morphingFonction/MatriceImg.java
@@ -3,13 +3,14 @@ package morphingFonction;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.Arrays;
+import java.io.IOException;
 
 import javax.imageio.ImageIO;
 public class MatriceImg {
 	private Color[][] matriceCouleur;
+	private Point[] tabPoint;
 	
-	public MatriceImg(File f) throws Exception {
+	public MatriceImg(File f, Point[] tabPoint) throws Exception {
 		BufferedImage img = ImageIO.read(f);
 		matriceCouleur = new Color[img.getHeight()][img.getWidth()];
 		for (int y = 0; y < img.getHeight(); y++) {
@@ -17,6 +18,7 @@ public class MatriceImg {
 				matriceCouleur[y][x] = new Color(img.getRGB(x,y), true);
 		    }
 		}
+		this.tabPoint = tabPoint; 
 	}
 
 	public Color[][] getMatrice() {
@@ -26,8 +28,14 @@ public class MatriceImg {
 	public void setMatrice(Color[][] matriceCouleur) {
 		this.matriceCouleur = matriceCouleur;
 	}
-	
 
+	public Point[] getTabPoint() {
+		return tabPoint;
+	}
+
+	public void setTabPoint(Point[] tabPoint) {
+		this.tabPoint = tabPoint;
+	}
 
 	@Override
 	public String toString() {
@@ -41,5 +49,39 @@ public class MatriceImg {
 		return r;
 	}
 	
+	public void creerImage() {
+		BufferedImage image = new BufferedImage(matriceCouleur[0].length, matriceCouleur.length, BufferedImage.TYPE_INT_RGB);
+		for (int y = 0; y < matriceCouleur.length; y++) {
+			for (int x = 0; x < matriceCouleur[0].length; x++) {
+				image.setRGB(x, y, matriceCouleur[y][x].getRGB());
+		    }
+		}
+		try {
+			ImageIO.write(image, "JPG", new File("imgTest3.jpg"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
+	public void matriceSuivante(Point[] tabSuivant) {
+		for(int i = 0; i<tabSuivant.length; i++) {
+			if(this.tabPoint[i] != tabSuivant[i]) {
+				if(this.tabPoint[i].getX() != tabSuivant[i].getX()) {
+					if(this.tabPoint[i].getX() < tabSuivant[i].getX()) {
+						Color c = this.matriceCouleur[this.tabPoint[i].getY()][this.tabPoint[i].getX() - 1];
+						for (int y = 0; y < matriceCouleur.length; y++) {
+							for (int x = 0; x < matriceCouleur[0].length; x++) {
+								if(y <= Integer.max(this.tabPoint[i].getY(), this.tabPoint[i+1].getY()) && y >= Integer.min(this.tabPoint[i].getY(), this.tabPoint[i+1].getY()) 
+										&& x >= this.tabPoint[i].getX() + Math.round(((((double) this.tabPoint[i].getX() - this.tabPoint[i+1].getX()))/(((double) this.tabPoint[i].getY() - this.tabPoint[i+1].getY()))) * (y - Integer.min(this.tabPoint[i].getY(), this.tabPoint[i+1].getY())))
+										&& x <= tabSuivant[i].getX() + Math.round(((((double) tabSuivant[i].getX() - this.tabPoint[i+1].getX()))/((double) (tabSuivant[i].getY() - this.tabPoint[i+1].getY()))) * (y - Integer.min(this.tabPoint[i].getY(), this.tabPoint[i+1].getY())))) {
+									matriceCouleur[y][x] = c;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- Projet Java/src/morphingFonction/Teste.java
+++ Projet Java/src/morphingFonction/Teste.java
@@ -4,7 +4,10 @@ import java.io.File;
 
 public class Teste {
 	public static void main(String[] args) throws Exception {
-		MatriceImg m = new MatriceImg(new File("imgTest.jpg"));
+		MatriceImg m = new MatriceImg(new File("imgTest2.jpg"), new Point[]{new Point(2,2),new Point(2,7)});
 		System.out.println(m);
+		m.matriceSuivante(new Point[]{new Point(4,2),new Point(2,7)});
+		System.out.println(m);
+		m.creerImage();
 	}
 }

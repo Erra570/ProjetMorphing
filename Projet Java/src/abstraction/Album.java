4d013c58b3c3993f53ad41bda8b1e93aeccdefee
Charrier Simon <simon.charrier42@outlook.fr>
2024-05-17 07:43:05 +0200

    Changed starting process (no more asking for pictures at start)

--- Projet Java/src/abstraction/Album.java
+++ Projet Java/src/abstraction/Album.java
@@ -7,6 +7,16 @@ import javafx.scene.image.Image;
 import javafx.stage.FileChooser;
 import presentation.Fichier;
 
+/**
+ * Classe faisant la liaison entre les fichiers et les images
+ * 
+ * @author Reignier Arnaud
+ * @author Vigneron Bastien
+ * @author Finana Tom
+ * @author Fontaine Nicolas
+ * @author Charrier Simon
+ *
+ */
 @SuppressWarnings("deprecation")
 public class Album extends Observable {
 
@@ -15,38 +25,60 @@ public class Album extends Observable {
 	@SuppressWarnings("removal")
 	public static final Integer IMAGE_Fin = new Integer(1); // PAC
 	
-	private Image imageDepart;
-	private Image imageFin;
-	private Fichier f;
+	private Image imageDepart; // Image de départ
+	private Image imageFin; // Image de fin
+	private Fichier f; // Fichier image à ajouter
 
+	/**
+	 * Constructeur de la classe album
+	 * 
+	 * @param f fichier à ajouter
+	 */
 	public Album(Fichier f) {
-		FileChooser fc = new FileChooser();
-		File file = fc.showOpenDialog(null);
+		// à la création de l'album, on importe une image par défaut (le carrée gris) et on initialise les 2 images
+		File file = new File("img/default.png");
 		this.imageDepart = new Image(file.toURI().toString());
 		this.f = f;
 		f.setF(file);
-		
-		fc = new FileChooser();
-		file = fc.showOpenDialog(null);
 		this.imageFin = new Image(file.toURI().toString());
 	}
 
+	/**
+	 * Setteur de l'image de départ
+	 * 
+	 * @param fullpathname Chemin vers l'image à afficher
+	 */
 	public void setImageDepart(String fullpathname) {
 		this.imageDepart = new Image(fullpathname);
 		this.setChanged(); // PAC
 		this.notifyObservers(IMAGE_Depart); // PAC
 	}
 	
+	/**
+	 * Setteur de l'image de fin
+	 * 
+	 * @param fullpathname Chemin vers l'image à afficher
+	 */
 	public void setImageFin(String fullpathname) {
 		this.imageFin = new Image(fullpathname);
 		this.setChanged(); // PAC
 		this.notifyObservers(IMAGE_Fin); // PAC
 	}
 	
+	/**
+	 * Getteur de l'image de départ
+	 * 
+	 * @return L'image de départ
+	 */
 	public Image getImageDepart() {
 		return(this.imageDepart);
 	}
 	
+	/**
+	 * Getteur de l'image de fin
+	 * 
+	 * @return L'image d'arrivée
+	 */
 	public Image getImageFin() {
 		return(this.imageFin);
 	}

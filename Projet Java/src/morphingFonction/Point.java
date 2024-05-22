5bbb3d34cf11d2db07a8a1ed88a7adf9832d9fff
R4ik0 <raiko352003@gmail.com>
2024-05-22 09:43:37 +0200

    forme arrondie

--- Projet Java/src/morphingFonction/Point.java
+++ Projet Java/src/morphingFonction/Point.java
@@ -1,100 +1,90 @@
 package morphingFonction;
 
+import java.util.List;
+
+import first.QCurve;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.QuadCurveTo;
+import javafx.scene.shape.Circle;
 
 /**
- * Classe Point représentant un point en deux dimensions avec des coordonnées x et y.
- * Contient des méthodes pour manipuler et comparer des points.
+ * Classe représentant un point avec des coordonnées x et y.
+ * Fournit des méthodes pour déterminer la position relative des points et vérifier l'appartenance à des figures géométriques.
  */
 public class Point {
-	private int x;
-	private int y;
-	
-	/**
-     * Getter de la coordonnée x du point.
+    private int x;  // Coordonnée x du point
+    private int y;  // Coordonnée y du point
+
+    /**
+     * Getter pour la coordonnée x.
      * 
-     * @return coordonnée x du point
+     * @return la coordonnée x
      */
-	public int getX() {
-		return x;
-	}
-	
-	/**
-     * Setter de la coordonnée x du point.
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Setter pour la coordonnée x.
      * 
-     * @param x la nouvelle coordonnée x du point
+     * @param x la nouvelle coordonnée x
      */
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	/**
-     * Getter de la coordonnée y du point.
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Getter pour la coordonnée y.
      * 
-     * @return coordonnée y du point
+     * @return la coordonnée y
      */
-	public int getY() {
-		return y;
-	}
-	
-	/**
-     * Setter de la coordonnée y du point.
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Setter pour la coordonnée y.
      * 
-     * @param x la nouvelle coordonnée y du point
+     * @param y la nouvelle coordonnée y
      */
-	public void setY(int y) {
-		this.y = y;
-	}
-	
-	/**
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
      * Constructeur de la classe Point.
      * 
-     * @param x la coordonnée x du point
-     * @param y la coordonnée y du point
+     * @param x la coordonnée x
+     * @param y la coordonnée y
      */
-	public Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     /**
-     * Calcule la position relative de ce point par rapport à une droite définie par deux points.
+     * Calcule la position relative du point actuel par rapport aux points a et b.
+     * Utilise le produit vectoriel pour déterminer la position.
      * 
-     * @param a le premier point définissant la droite
-     * @param b le deuxième point définissant la droite
-     * @return la position du point par rapport à la droite
+     * @param a le point a
+     * @param b le point b
+     * @return la position relative
      */
-	public double position(Point a, Point b) {
-		return((((double) (a.getX() - this.getX())) * ((double) (b.getY() - this.getY()))) - (((double) (a.getY() - this.getY())) * ((double) (b.getX() - this.getX()))));
-	}
-	
-	/**
-     * Détermine si ce point est à l'intérieur d'un triangle défini par trois autres points.
+    public double position(Point a, Point b) {
+        return ((((double) (a.getX() - this.getX())) * ((double) (b.getY() - this.getY()))) 
+                - (((double) (a.getY() - this.getY())) * ((double) (b.getX() - this.getX()))));
+    }
+
+    /**
+     * Vérifie si le point actuel est à l'intérieur du triangle défini par les points a, b et c.
+     * Utilise la méthode de position relative pour chaque côté du triangle.
      * 
-     * @param a le premier sommet du triangle
-     * @param b le deuxième sommet du triangle
-     * @param c le troisième sommet du triangle
-     * @return true si ce point est à l'intérieur du triangle, false sinon
+     * @param a le point a
+     * @param b le point b
+     * @param c le point c
+     * @return true si le point est à l'intérieur du triangle, false sinon
      */
-	public boolean dansTriangle(Point a, Point b, Point c) {
-		return (((position(c,a) >= 0) && (position(a,b) >= 0) && (position(b,c) >= 0))
-				|| ((position(c,a) <= 0) && (position(a,b) <= 0) && (position(b,c) <= 0)));
-	}
-	
-	public boolean dansFigure(Point[] tab) {        
-        Path path = new Path();
-        path.getElements().add(new MoveTo(tab[0].getX(),tab[0].getY()));
-        path.getElements().add(new QuadCurveTo(tab[2].getX(),tab[2].getY(),tab[1].getX(),tab[1].getY()));
-        for(int i = 3; i<tab.length-1; i+=2) {
-            path.getElements().add(new QuadCurveTo(tab[i+1].getX(),tab[i+1].getY(),tab[i].getX(),tab[i].getY()));    
-        }
-        path.getElements().add(new QuadCurveTo(tab[tab.length-1].getX(),tab[tab.length-1].getY(),tab[0].getX(),tab[0].getY()));
-        
-        path.setFill(Color.TRANSPARENT);
-        
-        return path.contains(this.x,this.y);
+    public boolean dansTriangle(Point a, Point b, Point c) {
+        return (((position(c, a) >= 0) && (position(a, b) >= 0) && (position(b, c) >= 0)) 
+                || ((position(c, a) <= 0) && (position(a, b) <= 0) && (position(b, c) <= 0)));
     }
 }
--- Projet Java/src/morphingFonction/Test.java
+++ Projet Java/src/morphingFonction/Test.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import first.QCurve;
+
 /**
  * Classe Test contenant la méthode main pour tester le morphing d'images.
  */
@@ -17,29 +19,59 @@ public class Test {
      */
 	public static void main(String[] args) throws Exception {
 		
-		List<Point> tabD = new ArrayList<>();
-        tabD.add(0, new Point(2,2));
-        tabD.add(1, new Point(2,498));
-        tabD.add(2, new Point(2,250));
-        tabD.add(3, new Point(498,498));
-        tabD.add(4, new Point(250,498));
-        tabD.add(5, new Point(498,2));
-        tabD.add(6, new Point(498,250));
-        tabD.add(7, new Point(250,2));
+		List<QCurve> tabD = new ArrayList<>();
+        tabD.add(0, new QCurve(2,2,2,2,2,498));
+        tabD.add(1, new QCurve(2,498,2,498,498,498));
+        tabD.add(2, new QCurve(498,498,498,498,498,2));
+        tabD.add(3, new QCurve(498,2,498,2,2,2));
         
-        List<Point> tabF = new ArrayList<>();
-        tabF.add(0, new Point(250,2));
-        tabF.add(1, new Point(2,250));
-        tabF.add(2, new Point(2,2));
-        tabF.add(3, new Point(250,498));
-        tabF.add(4, new Point(2,498));
-        tabF.add(5, new Point(498,250));
-        tabF.add(6, new Point(498,498));
-        tabF.add(7, new Point(498,2));
+        List<QCurve> tabF = new ArrayList<>();
+        tabF.add(0, new QCurve(250,2,2,2,2,250));
+        tabF.add(1, new QCurve(2,250,2,498,250,498));
+        tabF.add(2, new QCurve(250,498,498,498,498,250));
+        tabF.add(3, new QCurve(498,250,498,2,250,2));
+        
+        double nombreImg = 60;
+        int i = 25;
+        
+        QCurve[] tabIncrement = new QCurve[tabD.size()];
+        for (int j = 0; j < tabD.size(); j++) {
+            double XDepart = (tabF.get(j).getXDepart() - tabD.get(j).getXDepart()) / nombreImg;
+            double YDepart = (tabF.get(j).getYDepart() - tabD.get(j).getYDepart()) / nombreImg;
+            double XControl = (tabF.get(j).getXControl() - tabD.get(j).getXControl()) / nombreImg;
+            double YControl = (tabF.get(j).getYControl() - tabD.get(j).getYControl()) / nombreImg;
+            double XFin = (tabF.get(j).getXFin() - tabD.get(j).getXFin()) / nombreImg;
+            double YFin = (tabF.get(j).getYFin() - tabD.get(j).getYFin()) / nombreImg;
+
+            tabIncrement[j] = new QCurve(XDepart, YDepart, XControl, YControl, XFin, YFin);
+        }
+
+        List<QCurve> tabSuivant = new ArrayList<>();
+        for (int j = 0; j < tabD.size(); j++) {
+            double XDepart = tabD.get(j).getXDepart() + (i * tabIncrement[j].getXDepart());
+            double YDepart = tabD.get(j).getYDepart() + (i * tabIncrement[j].getYDepart());
+            double XControl = tabD.get(j).getXControl() + (i * tabIncrement[j].getXControl());
+            double YControl = tabD.get(j).getYControl() + (i * tabIncrement[j].getYControl());
+            double XFin = tabD.get(j).getXFin() + (i * tabIncrement[j].getXFin());
+            double YFin = tabD.get(j).getYFin() + (i * tabIncrement[j].getYFin());
+
+            if (j == 0) {
+                tabSuivant.add(new QCurve(XDepart, YDepart, XControl, YControl, XFin, YFin));
+            } else {
+                tabSuivant.add(new QCurve(tabSuivant.get(j-1).getXFin(), tabSuivant.get(j-1).getYFin(), XControl, YControl, XFin, YFin));
+                System.out.println(XDepart + "," + tabSuivant.get(j).getXFin() + "," + XFin);
+            }
+        }
+        /*tabF.add(0, new QCurve(250,2,2,2,2,250));
+        tabF.add(0, new QCurve(2,250,2,498,250,498));
+        tabF.add(0, new QCurve(250,498,498,498,498,250));
+        tabF.add(0, new QCurve(498,250,498,2,250,2));*/
 		
 		MorphingImg m = new MorphingImg(new File("img/gImg.jpg"), tabD);
 		
-		m.creerGif(tabD, tabF, 60);
+		m.imgSuivanteFormeArrondie(tabSuivant);
+		m.creerImage();
+		//m.creerGif(tabD, tabF, 60);
 		
 		System.out.println("Traitement terminé!");
 	}

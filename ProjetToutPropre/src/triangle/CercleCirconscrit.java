4f8fa2361f086cc2eb0617f9d4d0e67db907ad8f
heurkenn <57776002+heurkenn@users.noreply.github.com>
2024-05-27 09:26:14 +0200

    Add files via upload

--- ProjetToutPropre/src/triangle/CercleCirconscrit.java
+++ ProjetToutPropre/src/triangle/CercleCirconscrit.java
@@ -16,4 +16,21 @@ public class CercleCirconscrit {
 		double distancePointCentre = distanceX * distanceX + distanceY * distanceY;
 		return distancePointCentre <= rayon*rayon;
 	}
+
+	public Point getCentre() {
+		return centre;
+	}
+
+	public void setCentre(Point centre) {
+		this.centre = centre;
+	}
+
+	public double getRayon() {
+		return rayon;
+	}
+
+	public void setRayon(double rayon) {
+		this.rayon = rayon;
+	}
+	
 }
--- ProjetToutPropre/src/triangle/Triangle.java
+++ ProjetToutPropre/src/triangle/Triangle.java
@@ -1,24 +1,57 @@
 package triangle;
 
 public class Triangle {
-	private Cote cote_1;
-	private Cote cote_2;
-	private Cote cote_3;
 	private Point point_1;
 	private Point point_2;
 	private Point point_3;
 	private CercleCirconscrit cercle;
-	
-	public Triangle(Cote c1, Cote c2, Cote c3) {
-		this.cote_1=c1;
-		this.cote_2=c2;
-		this.cote_3=c3;
-		this.point_1=c1.getDebut();
-		this.point_2=c2.getDebut();
-		this.point_3=c3.getDebut();
+
+	public Triangle(Point p1, Point p2, Point p3) throws Exception {
+		this.point_1 = p1;
+		this.point_2 = p2;
+		this.point_3 = p3;
+		this.cercle = calculCercleCirconscrit();
 	}
-	
-	private CercleCirconscrit calculCercleCirconscrit() {
-		double a = point_1.getX()*point_1.getX() + point_1.getY()*point_1.getY();
+
+	private CercleCirconscrit calculCercleCirconscrit() throws Exception {
+		double a = point_1.getX() * point_1.getX() + point_1.getY() * point_1.getY();
+		double b = point_2.getX() * point_2.getX() + point_2.getY() * point_2.getY();
+		double c = point_3.getX() * point_3.getX() + point_3.getY() * point_3.getY();
+		double delta = point_1.getX() * (point_2.getY() - point_3.getY())
+				- point_1.getY() * (point_2.getX() - point_3.getX())
+				+ (point_2.getX() * point_3.getY() - point_2.getY() * point_3.getX());
+		if (delta == 0) {
+			throw new Exception("Les points sont collinéaires");
+		}
+		double detCentreX = a * (point_2.getY() - point_3.getY()) - point_1.getY() * (b - c)
+				+ (b * point_3.getY() - point_2.getY() * c);
+		double detCentreY = a * (point_2.getX() - point_3.getX()) - point_1.getX() * (b - c)
+				+ (b * point_3.getX() - point_2.getX() * c);
+		double xCentre = detCentreX / (2 * delta);
+		double yCentre = -detCentreY / (2 * delta);
+		
+
+		Point centre = new Point(xCentre, yCentre);
+		
+		double rayon = Math.abs(point_1.distance(point_2)*point_2.distance(point_3)*point_3.distance(point_1)/(2*delta));
+
+		return new CercleCirconscrit(centre, rayon);
 	}
+
+	public CercleCirconscrit getCercle() {
+		return this.cercle;
+	}
+
+	public Point getPoint_1() {
+		return point_1;
+	}
+
+	public Point getPoint_2() {
+		return point_2;
+	}
+
+	public Point getPoint_3() {
+		return point_3;
+	}
+
 }

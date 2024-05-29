f2f205e6721a4562ec82cb1279c3f60c9f9b8fc3
R4ik0 <raiko352003@gmail.com>
2024-05-29 13:23:36 +0200

    visage qui marche normalement

--- Projet Java/src/morphingFonction/MorphingImgVisage.java
+++ Projet Java/src/morphingFonction/MorphingImgVisage.java
@@ -9,33 +9,30 @@ import java.util.Set;
 
 import javax.imageio.ImageIO;
 
-import presentation.Fichier;
-
 import java.awt.Color;
 import triangle.Point;
 import triangle.Triangle;
 
 /**
- * Classe MorphingImg représentant une image sous forme d'une ArrayList de points associés pour le morphing.
- * Permet de charger une image à partir d'un fichier et de manipuler les points associés.
+ * Classe MorphingImgVisage représentant une image avec des points associés pour le morphing.
+ * Permet de charger une image à partir d'un fichier et de manipuler les points de morphing associés.
  */
 public class MorphingImgVisage {
     private BufferedImage img;  // Image chargée
-    private List<Triangle> tabPoint;  // Liste des points associés à l'image pour le morphing
+    private List<Triangle> tabPoint;  // Liste des triangles de points associés pour le morphing
 
     /**
-     * Constructeur de la classe MorphingImg
+     * Constructeur de la classe MorphingImgVisage
      * 
      * @param f le fichier image à charger
-     * @param tabPoint la liste des points de morphing associés à l'image
+     * @param tabPoint la liste des triangles de points de morphing associés à l'image
+     * @throws IOException en cas d'erreur de lecture de l'image
      */
-    public MorphingImgVisage(Fichier f, List<Triangle> tabPoint) throws Exception {
-        this.img = ImageIO.read(f.getF());  // Lecture de l'image à partir du fichier
-        this.tabPoint = new ArrayList<>();
-        for(int i = 0; i<tabPoint.size(); i++) {
-            this.tabPoint.add(tabPoint.get(i));  // Copie des points de morphing
-        }
+    public MorphingImgVisage(File f, List<Triangle> tabPoint) throws IOException {
+        this.img = ImageIO.read(f);  // Lecture de l'image à partir du fichier
+        this.tabPoint = new ArrayList<>(tabPoint);  // Copie des points de morphing
     }
+
     /**
      * Getter de l'image.
      * 
@@ -55,27 +52,27 @@ public class MorphingImgVisage {
     }
 
     /**
-     * Getter de la liste des points de morphing.
+     * Getter de la liste des triangles de points de morphing.
      * 
-     * @return la liste
+     * @return la liste des triangles
      */
     public List<Triangle> getTabPoint() {
         return tabPoint;
     }
 
     /**
-     * Setter de la liste des points de morphing.
+     * Setter de la liste des triangles de points de morphing.
      * 
-     * @param tabPoint liste des points de morphing
+     * @param tabPoint liste des triangles de points de morphing
      */
     public void setTabPoint(List<Triangle> tabPoint) {
         this.tabPoint = tabPoint;
     }
 
     /**
-     * Crée une nouvelle image en enregistrant l'image actuelle dans un fichier.
+     * Enregistre l'image actuelle dans un fichier.
      */
-    public void creerImage() {
+    public void creerImage(BufferedImage img) {
         try {
             ImageIO.write(img, "JPG", new File("img/imgTest5.jpg"));
         } catch (IOException e) {
@@ -84,116 +81,134 @@ public class MorphingImgVisage {
     }
 
     /**
-     * Met à jour les points de l'image en fonction d'un tableau de nouveaux points et ajuste les pixels de l'image
-     * en fonction d'une forme arrondie définie par ces points.
+     * Met à jour les points de l'image en fonction d'une liste de nouveaux triangles
+     * et ajuste les pixels de l'image en conséquence.
      * 
-     * @param tabSuivant le tableau de nouveaux points de morphing
+     * @param tabSuivant la liste de nouveaux triangles de morphing
+     * @param tabFin la liste des triangles de fin pour le morphing
+     * @param imgFin l'image finale pour le morphing
+     * @param t le paramètre de transition (entre 0 et 1)
      */
-    public void imgSuivanteVisage(List<Triangle> tabSuivant, List<Triangle> tabFin, BufferedImage imgFin, double t) {
-    	BufferedImage imgSuivant = new BufferedImage(this.img.getWidth(), this.img.getHeight(), BufferedImage.TYPE_INT_RGB);
-    	for (int y = 0; y < img.getHeight(); y++) {
-			for (int x = 0; x < img.getWidth(); x++) {
-				for (int i = 0; i<tabPoint.size(); i++) {
-					PointMorphing p = new PointMorphing(x,y);
-					if (p.dansTriangle(tabPoint.get(i).getPoint_1().transtypage(), tabPoint.get(i).getPoint_2().transtypage(), tabPoint.get(i).getPoint_3().transtypage()))  {
-						Set<Point> ensembleP = tabPoint.get(i).pointNouveauTriangle(new Point(x,y), tabSuivant.get(i));
-						Set<Point> ensembleFin = tabPoint.get(i).pointNouveauTriangle(new Point(x,y), tabSuivant.get(i));
-						Color cFin = couleurMoyenne(imgFin, ensembleFin);
-						Color cPoint = new Color(img.getRGB((int) p.getX(), (int) p.getY()), true);
-				        int r = (int) ((1 - t) * cPoint.getRed() + t * cFin.getRed());
-				        int g = (int) ((1 - t) * cPoint.getGreen() + t * cFin.getGreen());
-				        int b = (int) ((1 - t) * cPoint.getBlue() + t * cFin.getBlue());
-				        Color c = new Color(r,g,b);
-				        for(Point e : ensembleP) {
-				    		imgSuivant.setRGB((int) e.getX(), (int) e.getY(), c.getRGB());
-				    	}
-					}
-				}
-			}
-    	}
-    	this.img = imgSuivant;
+    public BufferedImage imgSuivanteVisage(List<Triangle> tabSuivant, List<Triangle> tabFin, BufferedImage imgFin, double t) {
+        BufferedImage imgSuivant = new BufferedImage(this.img.getWidth(), this.img.getHeight(), BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                for (int i = 0; i < tabPoint.size(); i++) {
+                    PointMorphing p = new PointMorphing(x, y);
+                    if (p.dansTriangle(tabPoint.get(i).getPoint_1().transtypage(), tabPoint.get(i).getPoint_2().transtypage(), tabPoint.get(i).getPoint_3().transtypage())) {
+                        Set<Point> ensembleP = tabPoint.get(i).pointNouveauTriangle(new Point(x, y), tabSuivant.get(i));
+                        Set<Point> ensembleFin = tabPoint.get(i).pointNouveauTriangle(new Point(x, y), tabFin.get(i));
+                        Color cFin = couleurMoyenne(imgFin, ensembleFin);
+                        Color cPoint = new Color(img.getRGB((int) p.getX(), (int) p.getY()), true);
+                        int r = (int) ((1 - t) * cPoint.getRed() + t * cFin.getRed());
+                        int g = (int) ((1 - t) * cPoint.getGreen() + t * cFin.getGreen());
+                        int b = (int) ((1 - t) * cPoint.getBlue() + t * cFin.getBlue());
+                        Color c = new Color(r, g, b);
+                        for (Point e : ensembleP) {
+                            imgSuivant.setRGB((int) e.getX(), (int) e.getY(), c.getRGB());
+                        }
+                    }
+                }
+            }
+        }
+        return imgSuivant;
     }
-    
+
+    /**
+     * Calcule la couleur moyenne des pixels d'un ensemble de points dans une image.
+     * 
+     * @param img l'image source
+     * @param ensemble l'ensemble des points
+     * @return la couleur moyenne
+     */
     public Color couleurMoyenne(BufferedImage img, Set<Point> ensemble) {
-    	int r = 0;
-    	int g = 0;
-    	int b = 0;
-    	for(Point p : ensemble) {
-    		Color c = new Color(img.getRGB((int) p.getX(), (int) p.getY()), true);
-        	r += c.getRed();
+        int r = 0;
+        int g = 0;
+        int b = 0;
+        for (Point p : ensemble) {
+            Color c = new Color(img.getRGB((int) p.getX(), (int) p.getY()), true);
+            r += c.getRed();
             g += c.getGreen();
             b += c.getBlue();
-    	}
-    	return new Color(r/ensemble.size(), g/ensemble.size(), b/ensemble.size());
+        }
+        return new Color(r / ensemble.size(), g / ensemble.size(), b / ensemble.size());
     }
-    
-    
+
+    /**
+     * Calcule la position moyenne entre deux points en fonction du paramètre de transition t.
+     * 
+     * @param pDebut le point de départ
+     * @param pFin le point de fin
+     * @param t le paramètre de transition (entre 0 et 1)
+     * @return le point moyen
+     */
     private Point calculMoyenneCoord(Point pDebut, Point pFin, double t) {
         double x = Math.round((1 - t) * pDebut.getX() + t * pFin.getX());
         double y = Math.round((1 - t) * pDebut.getY() + t * pFin.getY());
         return new Point(x, y);
     }
-    
-    
+
     /**
+     * Calcule la liste des triangles interpolés entre deux listes de triangles en fonction du paramètre de transition t.
      * 
-     * @param tabD liste des points de départ
-     * @param tabF liste des points de fin
-     * @param t entre 0 et 1
-     * @return la moyenne des deux tableaux
-     * @throws Exception 
+     * @param tabD liste des triangles de départ
+     * @param tabF liste des triangles de fin
+     * @param t le paramètre de transition (entre 0 et 1)
+     * @return la liste des triangles interpolés
+     * @throws Exception en cas d'erreur
      */
     public List<Triangle> triangleMoyenne(List<Triangle> tabD, List<Triangle> tabF, double t) throws Exception {
-    	List<Triangle> res = new ArrayList<>();
+        List<Triangle> res = new ArrayList<>();
         for (int i = 0; i < tabD.size(); i++) {
             Triangle triD = tabD.get(i);
             Triangle triF = tabF.get(i);
-            
             Point p1 = calculMoyenneCoord(triD.getPoint_1(), triF.getPoint_1(), t);
             Point p2 = calculMoyenneCoord(triD.getPoint_2(), triF.getPoint_2(), t);
             Point p3 = calculMoyenneCoord(triD.getPoint_3(), triF.getPoint_3(), t);
-            
             res.add(new Triangle(p1, p2, p3));
         }
         return res;
     }
-    
+
     /**
-     * Crée un GIF animé en interpolant entre deux ensembles de points de morphing.
+     * Crée un GIF animé en interpolant entre deux ensembles de triangles de morphing.
      * 
-     * @param tabD liste des points de départ
-     * @param tabF liste des points d'arrivée
-     * @param f fichier de l'image de fin
+     * @param tabD liste des triangles de départ
+     * @param tabF liste des triangles de fin
+     * @param f le fichier image de fin
      * @param nombreImg nombre d'images dans l'animation
-     * @throws Exception 
+     * @throws Exception en cas d'erreur
      */
-    public void creerGif(List<Triangle> tabD, List<Triangle> tabF, Fichier f, double nombreImg) throws Exception {
+    public void creerGif(List<Triangle> tabD, List<Triangle> tabF, File f, double nombreImg) throws Exception {
         AnimatedGifEncoder e = new AnimatedGifEncoder();
         e.start("img/testGif.gif");  // Début de la création du GIF
         e.setRepeat(0);  // Répéter l'animation en boucle
         e.setFrameRate(60);  // Définir la cadence d'images
 
+        BufferedImage imgSuivant = this.getImg();
+        
         // Ajout de quelques images initiales pour commencer le GIF
-        for(int i = 0; i < 10; i++) {
-            e.addFrame(this.getImg());
+        for (int i = 0; i < 10; i++) {
+            e.addFrame(imgSuivant);
         }
         
         // Création des images intermédiaires en interpolant les points
-        for(int i = 1; i <= nombreImg; i++) {
-            List<Triangle> tabSuivant = triangleMoyenne(tabD, tabF, (1/nombreImg) * i);
-            this.imgSuivanteVisage(tabSuivant, tabF, ImageIO.read(f.getF()), (1/nombreImg) * i);  // Mise à jour de l'image avec les nouveaux points                
-            e.addFrame(this.getImg());  // Ajout de l'image mise à jour au GIF
+        for (int i = 1; i <= nombreImg; i++) {
+            List<Triangle> tabSuivant = triangleMoyenne(tabD, tabF, (1 / nombreImg) * i);
+            imgSuivant = this.imgSuivanteVisage(tabSuivant, tabF, ImageIO.read(f), (1 / nombreImg) * i);  // Mise à jour de l'image avec les nouveaux points                
+            e.addFrame(imgSuivant);  // Ajout de l'image mise à jour au GIF
 
-            System.out.println((i*100)/nombreImg);  // Affichage de la progression
-            if (i == 20)
-            	this.creerImage();  // Création d'une image intermédiaire
+            System.out.println((i * 100) / nombreImg);  // Affichage de la progression
+            /*if (i == 20) {
+                this.creerImage();  // Création d'une image intermédiaire
+            }*/
         }
 
         // Ajout de quelques images finales pour finir le GIF
-        for(int i = 0; i < 10; i++) {
-            e.addFrame(this.getImg());
+        for (int i = 0; i < 10; i++) {
+            e.addFrame(imgSuivant);
         }
-        
+
         e.finish();  // Finalisation du GIF
     }
 }
--- Projet Java/src/morphingFonction/Teste.java
+++ Projet Java/src/morphingFonction/Teste.java
@@ -1,5 +1,6 @@
 package morphingFonction;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +18,14 @@ public class Teste {
 		l2.add(0, new Triangle(new Point(101, 101), new Point(2,498), new Point(498,2)));
 		List<Triangle> l3 = new ArrayList<>();
 		l3.add(0, new Triangle(new Point(200, 200), new Point(2,498), new Point(498,2)));
-		MorphingImgVisage m = new MorphingImgVisage(new File("gImg.jpg"), l1,start);
-		m.imgSuivanteVisage(l2, l3, ImageIO.read(new File("img/default.png")), 0.5);
-		m.creerImage();
+		List<Triangle> l4 = new ArrayList<>();
+		l4.add(0, new Triangle(new Point(150, 150), new Point(2,498), new Point(498,2)));
+		MorphingImgVisage m = new MorphingImgVisage(new File("gImg.jpg"), l1);
+		BufferedImage img;
+		//img = m.imgSuivanteVisage(l2, l3, ImageIO.read(new File("img/default.png")), 0.5);
+		//img = m.imgSuivanteVisage(l4, l3, ImageIO.read(new File("img/default.png")), 0.75);
+		//m.creerImage(img);
+		m.creerGif(l1,l3,new File("img/default.png"),60);
 		System.out.println("Fin");
 	}
 }
